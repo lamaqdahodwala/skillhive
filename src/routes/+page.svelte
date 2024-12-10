c797387d539f124cbbae98c11f134895de95b02c
TypicalDoor1 <123847012+TypicalDoor1@users.noreply.github.com>
2024-12-10 21:07:34 +0000

    schema

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,10 @@
-<script>
-import { PrismaClient } from '@prisma/client';
+<script context="module" lang="ts">
+  import { PrismaClient } from '@prisma/client';
+  const prisma = new PrismaClient();
 
-const prisma = new PrismaClient();
 
-let data = prisma.user.findMany()
-
+  
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<h1>{data}</h1>
+
+<h1>Users</h1>
