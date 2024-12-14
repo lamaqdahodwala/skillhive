41677170ad3712b58aef7b7bf50fe01bbddf9516
Lamaq Dahodwala <lamaq091008@gmail.com>
2024-12-14 17:30:24 -0500

    add form action for upvotes

--- src/routes/lesson/[id]/+page.server.ts
+++ src/routes/lesson/[id]/+page.server.ts
@@ -1,41 +1,76 @@
-import { prisma } from "$lib/db"
-import { error, redirect, type Actions } from "@sveltejs/kit"
-import type { PageServerLoad } from "./$types"
+import { prisma } from '$lib/db';
+import { error, redirect, type Actions } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async(event) => {
-  let lesson = await prisma.lesson.findUnique({
-    where: {
-      id: Number( event.params.id )
-    }
-  })
+export const load: PageServerLoad = async (event) => {
+	let lesson = await prisma.lesson.findUnique({
+		where: {
+			id: Number(event.params.id)
+		}
+	});
 
-  if (!lesson) throw error(404, "Lesson not found")
+	if (!lesson) throw error(404, 'Lesson not found');
 
-  return {
-    lesson: lesson
-  }
-}
+	return {
+		lesson: lesson
+	};
+};
 
 export const actions: Actions = {
-  upvote: async(event) => {
-    let user = await event.locals.auth()
+	upvote: async (event) => {
+		let user = await event.locals.auth();
 
-    if (!user?.user){
-      throw redirect(302, "/signin")
-    }
-    
-    let id = Number(event.params.id)
+		if (!user?.user) {
+			throw redirect(302, '/signin');
+		}
 
-    await prisma.lesson.update({
-      where: {
-        id: id
-      },
-      data: {
-        upvotes: {
-          
-        }
+		let id = Number(event.params.id);
+
+		const userId = Number(user.user.id);
+
+		let alreadyVoted = await prisma.user.findUnique({
+			where: {
+				id: userId
+			},
+			include: {
+				lessonVotes: {
+					where: {
+						id: id
+					}
+				}
+			}
+		});
+
+		if (alreadyVoted?.lessonVotes.length > 0) {
+			await prisma.lesson.update({
+				where: {
+					id: id
+				},
+				data: {
+					upvotes: {
+						disconnect: { id: userId }
+					}
+				}
+			});
+
+      return {
+        success: true
       }
-    })
-    
-  }
-}
+		}
+
+		await prisma.lesson.update({
+			where: {
+				id: id
+			},
+			data: {
+				upvotes: {
+					connect: { id: userId }
+				}
+			}
+		});
+
+    return {
+      success: true
+    }
+	}
+};
