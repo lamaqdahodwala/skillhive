6a2bcc1ecb590637e808978ac79728c0b596cadc
Lamaq Dahodwala <lamaq091008@gmail.com>
2024-12-14 17:42:46 -0500

    finish ui for voting

--- src/routes/lesson/[id]/+page.svelte
+++ src/routes/lesson/[id]/+page.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
+
 	let { data } = $props();
 </script>
 
 <h1 class="text-3xl font-bold">
 	ID: {data.lesson.title}
 </h1>
-
-<p>{data.hasVoted}</p>
+<form method="post" action="?/upvote" use:enhance>
+	<button class="m-2 rounded-lg { data.hasVoted ? 'bg-red-200' :"bg-blue-200" } p-1 px-5 font-bold transition-all">
+		{#if !data.hasVoted}
+			Vote
+		{:else}
+			Remove vote
+		{/if}
+	</button>
+</form>
 
 <hr />
 
