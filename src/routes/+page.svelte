747dafaf07c78e352213ee77bcb4064d2096219b
TypicalDoor1 <123847012+TypicalDoor1@users.noreply.github.com>
2024-12-14 22:50:19 +0000

    page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,5 +42,48 @@
         
       </div>
   </div>
-  
+  <div class = " bg-[#0f0e17] h-screen flex w-screen">
+    <div class = "flex space-x-52">
+      <div class = "py-32 flex flex-col justify-center space-y-5 px-32">
+        <p class = "text-[#e9e3e3] font-bold text-5xl text-wrap ">Features!</p>
+        <p class = "text-[#fffffe] font-normal max-w-96">The Features Page highlights SkillHive's innovative tools designed to empower creators and engage learners. From lesson creation and AI-powered summaries to interactive quizzes and a split-screen notebook, explore how our platform transforms education into an interactive, collaborative experience.</p>
+        <button class="bg-slate-900 text-[#fffefe] px-10 py-3 rounded-md text-xl hover:text-[#0f0e17] font-bold hover:bg-[#FFA62F] -mx-1.5">Lesson Creation</button>
+        <button class="bg-slate-900 text-[#fffefe] px-10 py-3 rounded-md text-xl hover:text-[#0f0e17] font-bold hover:bg-[#FFA62F] -mx-1.5">Upvote & Recommendation System</button>
+        <button class="bg-slate-900 text-[#fffefe] px-10 py-3 rounded-md text-xl hover:text-[#0f0e17] font-bold hover:bg-[#FFA62F] -mx-1.5">Search & Filter System</button>
+        <button class="bg-slate-900 text-[#fffefe] px-10 py-3 rounded-md text-xl hover:text-[#0f0e17] font-bold hover:bg-[#FFA62F] -mx-1.5">Notebook Functionality</button>
+        <button class="bg-slate-900 text-[#fffefe] px-10 py-3 rounded-md text-xl hover:text-[#0f0e17] font-bold hover:bg-[#FFA62F] -mx-1.5">AI-Powered Summaries</button>
+        <button class="bg-slate-900 text-[#fffefe] px-10 py-3 rounded-md text-xl hover:text-[#0f0e17] font-bold hover:bg-[#FFA62F] -mx-1.5">Video Integration</button>
+        <button class="bg-slate-900 text-[#fffefe] px-10 py-3 rounded-md text-xl hover:text-[#0f0e17] font-bold hover:bg-[#FFA62F] -mx-1.5">Interactive Tools/Editor</button>
+      </div>
+      <div class = "py-32 px-32 ">
+        <p class = "text-[#e9e3e3] font-bold text-3xl text-wrap ">Lesson Creation</p>
+        <p class = "text-[#fffffe] font-normal max-w-96">Lorem ipsum odor amet, consectetuer adipiscing elit. Vel feugiat eu nibh; suspendisse quam ante. Auctor ac fermentum senectus nulla ad. Quam aliquam ultrices eu suscipit natoque a tortor. Eget blandit commodo molestie vestibulum erat pharetra vitae leo. Platea velit auctor pellentesque, viverra integer nostra suspendisse. Pharetra eleifend sit vel mattis netus pharetra enim. Quam sapien congue feugiat massa hac. Mattis maecenas dis sem imperdiet condimentum iaculis tempor. Eros faucibus per neque primis arcu lorem et sem. Lectus dapibus nam risus mattis ligula lacinia. Cras in litora nam aliquet, nec primis ultrices venenatis massa. Consectetur cursus vitae malesuada turpis tincidunt vestibulum felis feugiat. Erat ipsum fermentum vel a nam ultrices semper. </p>
+      </div>
+    </div>
+    
+</div>
+ 
+
+<footer class="bg-[white] rounded-lg shadow dark:bg-gray-900 m-4">
+  <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+      <div class="sm:flex sm:items-center sm:justify-between">
+          <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+              <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+              <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SkillHive</span>
+          </a>
+          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+              <li>
+                  <a href="#" class="hover:underline me-4 md:me-6">About</a>
+              </li>
+              <li>
+                  <a href="#" class="hover:underline">Contact</a>
+              </li>
+          </ul>
+      </div>
+      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://localhost:5173/" class="hover:underline">SkillHive</a>. All Rights Reserved.</span>
+  </div>
+</footer>
+
+
   
