b0c1bdcaab5e71cf2f5d091216e665b52b1ec3a1
TypicalDoor1 <123847012+TypicalDoor1@users.noreply.github.com>
2024-12-14 02:10:12 +0000

    home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,47 @@
 <script lang="ts">
     
 </script>
+
+
+<div class = "bg-[#0f0e17] min-h-screen w-screen">
+    <nav class = "min-w-screen flex items-center justify-between py-12">
+      <div class = "">
+        <p class = "text-[#fffffe] font-bold text-xl px-32 my-3"><span class="text-[#fffffe]">Skill</span><span class="text-[#FFA62F]">Hive</span></p>
+      </div>  
+      <div class ="flex justify-self-end justify-evenly space-x-5 px-32">
+        <!-- svelte-ignore a11y_missing_attribute -->
+        <a class = "text-[#fffffe] font-bold text-sm hover:text-[#FFA62F] hover:cursor-pointer">Home</a>
+        <a class = "text-[#fffffe] font-bold text-sm hover:text-[#FFA62F] hover:cursor-pointer">Log-In</a>
+        <!-- svelte-ignore a11y_missing_attribute -->
+        <a class = "text-[#fffffe] font-bold text-sm hover:text-[#FFA62F] hover:cursor-pointer">Sign-Up</a>
+      <div>
+    </nav>
+    <div class = "py-12 px-32  flex">
+      <div class = "space-y-52">
+        <div class="space-y-10">
+        <p class = "text-[#fffefe] font-bold text-5xl text-wrap">Study <span class="text-[#FFA62F]">Smarter</span>, Achieve <span class="text-[#FFA62F]">Greater</span>!</p>
+        <p class = "text-[#a7a9be] font-light text-lg w:1/2 tracking-widest lg:w-1/2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p> 
+        
+      </div>
+      <button class="bg-[#fffffe] px-20 py-3 rounded-md text-lg text-[#0f0e17] font-bold hover:bg-[#FFA62F]">Get Started!</button>
+    </div>
+  
+    
+    <!-- svelte-ignore a11y_missing_attribute -->
+    <img src="/honeycomb.png" class="relative -top-10" />
+  
+    </div>
+  
+  </div>
+  <div class = " bg-[#e9e3e3] h-96 flex w-screen ">
+      <div class = "flex space-x-96">
+        <div class = "py-32 flex flex-col justify-center space-y-5 px-32">
+          <p class = "text-[#0f0e17] font-bold text-5xl text-wrap ">About Us!</p>
+          <p class = "text-[#212129] font-normal max-w-96">We are all highschool students, who are <span class="text-slate-900 font-bold">passionate about academics, technology, and creative problem-solving.</span> Whether it’s designing tools to make studying easier or exploring innovative ideas, we enjoy taking on projects that challenge us to learn and grow.</p> 
+          <button class="hover:bg-slate-900 hover:text-[#fffefe] px-20 py-3 rounded-md text-lg text-[#0f0e17] font-bold bg-[#FFA62F] -mx-1.5">Learn More!</button>
+        </div>
+        
+      </div>
+  </div>
+  
+  
