71f835e5174966b38cd28162e91541bf24b45083
Lamaq Dahodwala <lamaq091008@gmail.com>
2024-12-15 11:08:49 -0500

    add links to home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,9 +7,9 @@
 
 <nav class="min-w-screen flex items-center justify-between bg-[#0f0e17] py-12">
 	<div class="">
-		<p class="my-3 px-32 text-xl font-bold text-[#fffffe]">
+		<a href="/" class="my-3 px-32 text-xl font-bold text-[#fffffe]">
 			<span class="text-[#fffffe]">Skill</span><span class="text-[#FFA62F]">Hive</span>
-		</p>
+		</a>
 	</div>
 	<div>
 		<form action="/search" method="get">
@@ -23,7 +23,7 @@
 	</div>
 	<div class="flex justify-evenly space-x-5 justify-self-end px-32">
 		<!-- svelte-ignore a11y_missing_attribute -->
-		<a class="text-sm font-bold text-[#fffffe] hover:cursor-pointer hover:text-[#FFA62F]">Home</a>
+		<a class="text-sm font-bold text-[#fffffe] hover:cursor-pointer hover:text-[#FFA62F]" href="/">Home</a>
 		<!-- svelte-ignore a11y_missing_attribute -->
     {#if data.user}
       <button class="text-sm bg-green-900 bg-opacity-50 px-3 rounded-lg font-bold text-[#fffffe] hover:cursor-pointer hover:text-[#FFA62F]">{data.user.name}</button>
